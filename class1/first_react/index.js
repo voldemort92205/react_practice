986d677a3b6b880e54e3718a0271cf5c949d64fe
perry92205 <voldemort92205@gmail.com>
2023-10-08 19:00:55 +0800

    Update API with react 18

--- class1/first_react/index.js
+++ class1/first_react/index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById("root"))
+
+const hello = (
+    <div>
+    <h1>Hello World!!</h1>
+    <p>the paragraph</p>
+    </div>
+);
+
+//ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById("root"))
+//ReactDOM.createRoot(document.getElementById("root")).render(hello);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(hello);
