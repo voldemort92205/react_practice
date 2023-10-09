1cd80bcc270e0ef1be5102070a8578786aa0624f
perry92205 <voldemort92205@gmail.com>
2023-10-09 19:27:29 +0800

    task 2 for week 1: Navbar & Styling
    Requirement:
    Challenge: Complete the Navbar to match the design
    
    Hint: use the Figma file for the most accurate peek at the design
    (colors, sizes, fonts, etc.)

--- class1/week1-project/src/Navbar.js
+++ class1/week1-project/src/Navbar.js
@@ -2,6 +2,14 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <h1>Navbar component</h1>
+        <div className="nav-container">
+            <div className="nav-container-left">
+                <img src="react-icon-small.png" alt="react-icon-logo" />
+                <h3>ReactFacts</h3>
+            </div>
+            <div className="nav-container-right">
+                <h3>React Course - Project 1</h3>
+            </div>
+        </div>
     )
 }
