0e842bcb1355d91dcdbe2c218aed6e98cb6efb59
perry92205 <voldemort92205@gmail.com>
2023-10-09 17:05:47 +0800

    task 1 for the week 1
    Note: We may not follow the folder structure
    
    /**
    Challenge: Project setup
    
    - Create a `components` folder
    - Create the following components in separate files inside
      the components folder.  In each one, just render an `h1`
      with the name of the component (e.g. return <h1>Navbar goes here</h1>):
        - Navbar
        - Main
    - Create an App component outside the components folder (sibling to
      the index.js file)
        - Have App render the Navbar and Main components
    - Import and render the App component inside of index.js using ReactDOM
        - At this point you should have your "Navbar goes here" etc. showing up
          in the mini-browser.
    - Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
      Put the links to those fonts ABOVE the style.css link in index.html (Use
      the `<link/>` elements instead of the @import or npm options for getting
      the fonts. You may need to do some extra research to figure out how this
      works if you haven't done it before)
    */

---
+++ class1/week1-project/README.md
@@ -0,0 +1,4 @@
+# Note
+
+Refer here to create this npm package:
+https://dev.to/ivadyhabimana/how-to-create-a-react-app-without-using-create-react-app-a-step-by-step-guide-30nl
---
+++ class1/week1-project/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Main from "./Main"
+import Navbar from "./Navbar"
+
+export default function App () {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+
