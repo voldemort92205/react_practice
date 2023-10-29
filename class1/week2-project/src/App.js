f1af79847c4c49317df92ea5d7a5bbbdf3073931
Chin-Hui Lee <voldemort92205@gmail.com>
2023-10-29 15:31:02 +0800

    task 2 for week 2: Project setup: Hero
    Challenge: Build the Hero component.
    Check the Figma file for the design specifics.

--- class1/week2-project/src/App.js
+++ class1/week2-project/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar"
+import Hero from "./Hero"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ class1/week2-project/src/Hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PhotoGrid from "./images/photo-grid.png"
+
+export default function Hero() {
+    return (
+        <div className="hero-section">
+            <img src={PhotoGrid} alt="hero gallery" className="hero-gallery" />
+            <h1 className="hero-header">Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
