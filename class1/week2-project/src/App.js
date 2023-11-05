e0e9ae753b1f47a9dad96d240a54af0b2d867b0c
Chin-Hui Lee <voldemort92205@gmail.com>
2023-11-05 11:19:18 +0800

    task 5 for week 2: Project: Map experiences data into components
    Challenge:
    
    - import the array of data from data.js
    - map over the array to create <Card /> components
    - display the array of card components under the navbar
      (in place of the current <Card /> component)
    
    Note: We haven't styled the group of components yet, so they'll
    still be block elements, stacked vertically. We'll add styling later.

--- class1/week2-project/src/App.js
+++ class1/week2-project/src/App.js
@@ -1,21 +1,25 @@
 import Navbar from "./Navbar"
 import Hero from "./Hero"
 import Card from "./Card"
+import data from "./data.js"
 
 function App() {
+  const cardData = data.map((item) => {
+    return <Card
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            country={item.location}
+            title={item.title}
+            price={item.price}
+          />
+  })
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <div className="card-gallery">
-        <Card
-          img="katie-zaferes.png"
-          rating="5.0"
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-        />
+        {cardData}
       </div>
     </div>
   );
