f6f5879ecda33b1de9b00446bc63ab8b7ecdaf4a
Chin-Hui Lee <voldemort92205@gmail.com>
2023-11-05 11:30:40 +0800

    task 5 for week 2: Project: key prop

--- class1/week2-project/src/App.js
+++ class1/week2-project/src/App.js
@@ -6,6 +6,7 @@ import data from "./data.js"
 function App() {
   const cardData = data.map((item) => {
     return <Card
+            key={item.id}
             img={item.coverImg}
             rating={item.stats.rating}
             reviewCount={item.stats.reviewCount}
