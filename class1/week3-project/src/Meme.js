c0547a67bf6af95bb6e7ea5c574d4854226fdad2
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-02 13:53:46 +0800

    task 3 for week 3 Project: Add images to the meme generator
    Challenge: Save the random meme URL in state
    - Create new state called `memeImage` with an
      empty string as default
    - When the getMemeImage function is called, update
      the `memeImage` state to be the random chosen
      image URL
    - Below the div.form, add an <img /> and set the
      src to the new `memeImage` state you created

--- class1/week3-project/src/Meme.js
+++ class1/week3-project/src/Meme.js
@@ -1,22 +1,40 @@
 import React from "react"
+import memesData from "./memesData.js"
 
-export default function Header() {
+export default function Meme() {
+    const [memeInfo, setMemeInfo] = React.useState({url: "", alt: ""})
+
+    function randomMemeImage () {
+        const memeArray = memesData.data.memes
+        const newIndex= Math.floor(Math.random() * memeArray.length)
+        setMemeInfo(memeArray[newIndex])
+    }
     return (
-        <form className="form-container">
-            <input
-                type="text"
-                placeholder="left text"
-                className="form-input"
-            />
-            <input
-                type="text"
-                placeholder="right text"
-                className="form-input"
-            />
+        <div>
+            <div className="form-container">
+                <input
+                    type="text"
+                    placeholder="left text"
+                    className="form-input"
+                />
+                <input
+                    type="text"
+                    placeholder="right text"
+                    className="form-input"
+                />
 
-            <button className="form-button">
-                Get a new meme image 🖼
-            </button>
-        </form>
+                <button
+                    className="form-button"
+                    onClick={randomMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img
+                className="meme-image"
+                src={memeInfo.url}
+                alt={memeInfo.name}
+            />
+        </div>
     )
 }
