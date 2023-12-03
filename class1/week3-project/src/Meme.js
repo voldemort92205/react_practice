4b412dcdd7cfb8d831cb9917ff06cdcd75bb73d2
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-03 10:08:44 +0800

    add async/ await version in comments

--- class1/week3-project/src/Meme.js
+++ class1/week3-project/src/Meme.js
@@ -28,6 +28,26 @@ export default function Meme() {
         }
     }, [])
 
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+    /*
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
+    */
+
     function handleMemeText(event) {
         setMeme(prev => ({
             ...prev,
