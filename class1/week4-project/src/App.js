fb0ab015017263306d35a45f15f5c584497ead8d
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-03 10:54:43 +0800

    initial: week4 project

---
+++ class1/week4-project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          Hello week4!!
+      </header>
+    </div>
+  );
+}
+
+export default App;
