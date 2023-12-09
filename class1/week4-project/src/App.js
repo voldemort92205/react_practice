05bb9f553242b4309c16a4bdf3e87932d17f9a29
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-09 12:02:39 +0800

    task 5 for week4 project: Notes App: Delete note
    Challenge: complete and implement the deleteNote function
    
    Hints:
    1. What array method can be used to return a new
       array that has filtered out an item based
       on a condition?
    2. Notice the parameters being based to the function
       and think about how both of those parameters
       can be passed in during the onClick event handler

--- class1/week4-project/src/App.js
+++ class1/week4-project/src/App.js
@@ -41,6 +41,11 @@ export default function App() {
         })
     }
 
+    function deleteNote(event, noteId) {
+        event.stopPropagation()
+        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+    }
+
     function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
@@ -62,6 +67,7 @@ export default function App() {
                     currentNote={findCurrentNote()}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
+                    deleteNote={deleteNote}
                 />
                 {
                     currentNoteId &&
--- class1/week4-project/src/Sidebar.js
+++ class1/week4-project/src/Sidebar.js
@@ -4,13 +4,19 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+
+                <button 
+                    className="delete-btn"
+                    onClick={(event) => props.deleteNote(event, note.id)}
+                >
+                    <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
     ))
