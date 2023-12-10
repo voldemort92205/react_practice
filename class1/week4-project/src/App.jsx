9c12da51e9b8748aacfdd3f0f103c63cb7b1421c
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-10 11:22:23 +0800

    task 6 for week4 project: Notes App: createdAt and updatedAt
    Challenge:
    1. Add createdAt and updatedAt properties to the notes
       When a note is first created, set the `createdAt` and `updatedAt`
       properties to `Date.now()`. Whenever a note is modified, set the
       `updatedAt` property to `Date.now()`.
    
    2. TBA

--- class1/week4-project/src/App.jsx
+++ class1/week4-project/src/App.jsx
@@ -34,7 +34,9 @@ export default function App() {
 
     async function createNewNote() {
         const newNote = {
-            body: "# Type your markdown note's title here"
+            body: "# Type your markdown note's title here",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
         }
         const newNoteRef = await addDoc(notesCollection, newNote);
         setCurrentNoteId(newNoteRef.id);
@@ -42,7 +44,10 @@ export default function App() {
 
     async function updateNote(text) {
         const docRef = doc(db, "notes", currentNoteId)
-        await setDoc(docRef, { body: text }, { merge: true })
+        await setDoc(docRef,
+            { body: text, updatedAt: Date.now() },
+            { merge: true }
+        )
     }
 
     async function deleteNote(noteId) {
