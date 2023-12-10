f694bccc29534e7789f1b16eaf63f0bef0643444
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-10 10:34:55 +0800

    Firebase code setup

---
+++ class1/week4-project/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfhOWxcSXsvzPrj0NcRAXvfk7BZTOpKd0",
+  authDomain: "react-notes-d43d0.firebaseapp.com",
+  projectId: "react-notes-d43d0",
+  storageBucket: "react-notes-d43d0.appspot.com",
+  messagingSenderId: "116463089360",
+  appId: "1:116463089360:web:21fc495ae0ef482e2750d5"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes");
