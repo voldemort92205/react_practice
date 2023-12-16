bcabfa3ec5839562f44bfa5138abcbc5a50746ae
Chin-Hui Lee <voldemort92205@gmail.com>
2023-12-16 11:46:50 +0800

    task 3 for week4 project 2: Tenzies: Generate array of 10 random numbers
    Challenge:
    
    Write a function (allNewDice) that returns an array
    of 10 random numbers between 1-6 inclusive.
    
    Log the array of numbers to the console for now

--- class1/week4-project2/src/App.jsx
+++ class1/week4-project2/src/App.jsx
@@ -2,6 +2,15 @@ import React from "react"
 import Die from "./Die"
 
 export default function App() {
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+}
+console.log(allNewDice())
+
   return (
     <main>
       <div className="die-container">
